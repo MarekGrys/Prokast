32491dc82f71464c3178efa601a7216d4ad895d4
Jakub Reszczynski <reszczujprkuba@gmail.com>
2024-11-13 19:27:33 +0100

    .

--- Prokast.Klient/log_in/Masks.js
+++ Prokast.Klient/log_in/Masks.js
@@ -1,12 +1,11 @@
-// Funkcja inicjująca maski dla różnych pól
 function initializeMasks() {
-    // Maskowanie numeru telefonu (np. 123-456-789)
+    
     const phoneElement = document.getElementById('Phone_Number');
     if (phoneElement) {
         IMask(phoneElement, { mask: '000-000-000' });
     }
 
-    // Maskowanie kodu pocztowego (np. 12-345)
+    
     const postalCodeElement = document.getElementById('Postal_Code');
     if (postalCodeElement) {
         IMask(postalCodeElement, { mask: '00-000' });
@@ -14,5 +13,5 @@ function initializeMasks() {
 
 }
 
-// Inicjalizacja masek po załadowaniu dokumentu
+
 document.addEventListener('DOMContentLoaded', initializeMasks);
