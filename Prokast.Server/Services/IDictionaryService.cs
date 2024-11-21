686f800cef76f76ff966f9c0cc4baabce9536def
HubertCiura <147997409+HubertCiura@users.noreply.github.com>
2024-11-21 20:52:45 +0100

    Add files via upload

---
+++ Prokast.Server/Services/IDictionaryService.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Prokast.Server.Entities;
+using Prokast.Server.Models;
+
+namespace Prokast.Server.Services
+{
+    public interface IDictionaryService
+    {
+        
+        Response GetAllParams();
+        Response GetParamsByID( int ID);
+        Response GetParamsByName( string name);
+        
+    }
+}
