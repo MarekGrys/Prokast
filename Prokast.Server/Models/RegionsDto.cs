d9a45b14b5eed2b01a38fcc9da3acfed28319f13
HubertCiura <147997409+HubertCiura@users.noreply.github.com>
2024-11-21 20:53:25 +0100

    Add files via upload

---
+++ Prokast.Server/Models/RegionsDto.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Prokast.Server.Models
+{
+    public class RegionsDto
+    {
+        [Required]
+        public string Name { get; set; }
+        
+    }
+}
