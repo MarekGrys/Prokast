b5e1f45e7b2577188cc51b9817869aeaa541c0ac
Marek Grys <MarekGrys01@gmail.com>
2025-03-09 22:27:51 +0100

    Added EP taking minimal data for all warehouses for a client

---
+++ Prokast.Server/Models/ResponseModels/WarehouseResponseModels/WarehouseGetMinimalResponse.cs
@@ -0,0 +1,9 @@
+﻿using Prokast.Server.Models.WarehouseModels;
+
+namespace Prokast.Server.Models.ResponseModels.WarehouseResponseModels
+{
+    public class WarehouseGetMinimalResponse: Response
+    {
+        public List<WarehouseGetMinimal> Model { get; set; }
+    }
+}
