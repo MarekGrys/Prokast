495b8308ae634e81faf034507797f9906917b0a6
HubertCiura <147997409+HubertCiura@users.noreply.github.com>
2025-07-31 23:50:19 +0200

    Add files via upload

---
+++ Prokast.Server/Services/BlobPhotoStorageService.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Azure.Storage.Blobs;
+using Azure.Storage.Blobs.Models;
+using Prokast.Server.Services.Interfaces;
+using Prokast.Server.Models;
+using Microsoft.Extensions.Options;
+using Microsoft.VisualBasic;
+
+namespace Prokast.Server.Services
+{
+    public class BlobPhotoStorageService : IBlobPhotoStorageService
+    {
+        private readonly AzureBlobStorageSettings _blobStorageSettings;
+        public BlobPhotoStorageService(IOptions<AzureBlobStorageSettings> options)
+        {
+            _blobStorageSettings = options.Value;
+        }
+
+
+        //private readonly BlobServiceClient _blobServiceClient = new(Environment.GetEnvironmentVariable("StorageConnection"));
+
+        
+        /// <summary>
+        /// Funkcja odpowiedzialna za upload podanego zdjęcia do Blob-a
+        /// </summary>
+        /// <param name="photoName"></param>
+        /// <param name="containerName"></param>
+        /// <param name="photoData"></param>
+        /// <returns></returns>
+        public string UploadPhotoAsync(BLOBPhotoModel photo) {
+            var _blobServiceClient = new BlobServiceClient(_blobStorageSettings.StorageConnection);
+
+            var container = _blobServiceClient.GetBlobContainerClient("images");
+             container.CreateIfNotExists();
+
+            var blobClient = container.GetBlobClient(photo.Name);
+            
+            var upload = new BlobUploadOptions
+            {
+                HttpHeaders = new BlobHttpHeaders 
+                {
+                    ContentType = photo.Name.Contains(".jpg") ? "image/jpeg" : "image/png"
+                }
+            };
+
+            using var stream = new MemoryStream(photo.Value);
+            stream.Position = 0;
+            blobClient.Upload(stream, upload);
+            
+            return blobClient.Uri.ToString();
+        }
+
+        /// <summary>
+        /// Funkcja odpowiedzialna za pobranie wybranego zdjęcia z Blob-a
+        /// </summary>
+        /// <param name="photoName"></param>
+        /// <param name="containerName"></param>
+        /// <returns></returns>
+        /// <exception cref="FileNotFoundException"></exception>
+        public async Task<byte[]> DownloadPhotoAsync(string photoName) 
+        {
+            var _blobServiceClient = new BlobServiceClient(_blobStorageSettings.StorageConnection);
+
+            var container = _blobServiceClient.GetBlobContainerClient("images");
+            var blobClient = container.GetBlobClient(photoName);
+
+            if(!await blobClient.ExistsAsync())
+            {
+                throw new FileNotFoundException($"Plik '{photoName}' nie istnieje!");
+            }
+
+            var downloadInfo = await blobClient.DownloadAsync();
+            await using var stream = new MemoryStream();
+            await downloadInfo.Value.Content.CopyToAsync(stream);
+
+            return stream.ToArray();
+        }
+    }
+}
--- Prokast.Server/Services/PhotoService.cs
+++ Prokast.Server/Services/PhotoService.cs
@@ -5,8 +5,11 @@ using Prokast.Server.Entities;
 using Prokast.Server.Models;
 using Prokast.Server.Models.PhotoModels;
 using Prokast.Server.Models.ResponseModels;
+using Prokast.Server.Models.ResponseModels.CustomParamsResponseModels;
 using Prokast.Server.Models.ResponseModels.PhotoResponseModels;
 using Prokast.Server.Services.Interfaces;
+using System.Text;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace Prokast.Server.Services
 {
@@ -14,12 +17,47 @@ namespace Prokast.Server.Services
     {
         private readonly ProkastServerDbContext _dbContext;
         private readonly IMapper _mapper;
+        private readonly IBlobPhotoStorageService _blobPhotoStorageService;
         Random random = new Random();
 
-        public PhotoService(ProkastServerDbContext dbContext, IMapper mapper)
+        public PhotoService(ProkastServerDbContext dbContext, IMapper mapper, IBlobPhotoStorageService blobPhotoStorageService)
         {
             _dbContext = dbContext;
             _mapper = mapper;
+            _blobPhotoStorageService = blobPhotoStorageService;
+        }
+
+
+        public Response CreatePhoto([FromBody] PhotoDto photo, int clientID) { 
+            if(photo == null)
+            {
+                var responseNull = new ErrorResponse() { ID = random.Next(1, 100000), ClientID = clientID, errorMsg = "Błędnie podane dane" };
+                return responseNull;
+            }
+            byte[] ValueByte = Encoding.UTF8.GetBytes(photo.Value);
+            var photoBLOB = new BLOBPhotoModel
+            {
+                Name = photo.Name,
+                Value = ValueByte,
+            };
+            var link = _blobPhotoStorageService.UploadPhotoAsync(photoBLOB);
+            _blobPhotoStorageService.DownloadPhotoAsync(photo.Name);
+            
+            var newPhoto = new Photo { 
+                Name = photo.Name,
+                ClientID = clientID,
+                ProductID = photo.ProductId,
+                Value = link.ToString(),
+            };
+
+            _dbContext.Photos.Add(newPhoto);
+            _dbContext.SaveChanges();
+
+            
+            
+
+            var response = new Response() { ID = random.Next(1, 100000), ClientID = clientID };
+            return response;
         }
 
         #region Get
@@ -37,7 +75,7 @@ namespace Prokast.Server.Services
 
         public Response GetPhotosByID(int clientID, int ID)
         {
-            var param = _dbContext.Photos.Where(x => x.ClientID == clientID && x.Id == ID).ToList();
+            var param = _dbContext.Photos.Where(x => x.ClientID == clientID && x.ID == ID).ToList();
             var response = new PhotoGetResponse() { ID = random.Next(1, 100000), ClientID = clientID, Model = param };
             if (param.Count() == 0)
             {
@@ -47,12 +85,36 @@ namespace Prokast.Server.Services
             return response;
 
         }
+
+        public Response GetAllPhotosInProduct(int clientID, int productID)
+        {
+            var responseNull = new ErrorResponse() { ID = random.Next(1, 100000), errorMsg = "Nie ma takiego zdjecia" };
+
+            var product = _dbContext.Products.FirstOrDefault(x => x.ClientID == clientID && x.ID == productID);
+            if (product == null)
+            {
+                responseNull.errorMsg = "Nie ma takiego produktu!";
+                return responseNull;
+            }
+            var PhotosIDList = product.Photos.Split(",")
+                              .Select(x => int.Parse(x)).ToList();
+
+            var PhotosList = _dbContext.Photos.Where(x => PhotosIDList.Contains(x.ID)).ToList();
+            if (PhotosList.Count() == 0)
+            {
+                return responseNull;
+            }
+
+            var response = new PhotoGetResponse() { ID = random.Next(1, 100000), Model = PhotosList };
+            return response;
+
+        }
         #endregion
 
         #region Edit
         public Response EditPhotos(int clientID, int ID, PhotoEdit data)
         {
-            var findPhoto = _dbContext.Photos.FirstOrDefault(x => x.ClientID == clientID && x.Id == ID);
+            var findPhoto = _dbContext.Photos.FirstOrDefault(x => x.ClientID == clientID && x.ID == ID);
 
 
             if (findPhoto == null)
@@ -67,6 +129,8 @@ namespace Prokast.Server.Services
 
             var response = new PhotoEditResponse() { ID = random.Next(1, 100000), ClientID = clientID, photo = findPhoto };
 
+            
+
             return response;
         }
         #endregion
@@ -74,7 +138,7 @@ namespace Prokast.Server.Services
         #region delete
         public Response DeletePhotos(int clientID, int ID)
         {
-            var findPhoto = _dbContext.Photos.FirstOrDefault(x => x.ClientID == clientID && x.Id == ID);
+            var findPhoto = _dbContext.Photos.FirstOrDefault(x => x.ClientID == clientID && x.ID == ID);
 
 
             if (findPhoto == null)
