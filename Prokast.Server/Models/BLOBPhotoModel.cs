d7b8f97aa8adc3b4cf134175ed2e9bce41d8b944
HubertCiura <147997409+HubertCiura@users.noreply.github.com>
2025-07-31 23:53:12 +0200

    Add files via upload

---
+++ Prokast.Server/Models/BLOBPhotoModel.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace Prokast.Server.Models
+{
+    public class BLOBPhotoModel 
+    {
+        [Required] 
+        public string Name { get; set; }
+
+        [Required]
+        public byte[] Value { get; set; }
+    }
+}
